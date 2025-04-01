3e2205fa70dd78a43e26847fbe7b1b201d2d4517
Caner Tüysüz <canertuysuz@hotmail.com>
2025-04-01 12:15:17 +0300

    remove hexCode initializer and replace 'hexCode' with 'hex' property usage

--- Did You Today/Common/Extensions/UIColor+Extension.swift
+++ Did You Today/Common/Extensions/UIColor+Extension.swift
@@ -8,9 +8,8 @@
 import UIKit
 
 extension UIColor {
-    // Original initializer used in the app
-    convenience init(hexCode: String, alpha: CGFloat) {
-        var hexFormatted: String = hexCode.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).uppercased()
+    convenience init(hex: String) {
+        var hexFormatted: String = hex.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).uppercased()
         
         if hexFormatted.hasPrefix("#") {
             hexFormatted = String(hexFormatted.dropFirst())
@@ -25,34 +24,8 @@ extension UIColor {
             red: CGFloat((rgbColorValue & 0xFF0000) >> 16) / 255.0,
             green: CGFloat((rgbColorValue & 0x00FF00) >> 8) / 255.0,
             blue: CGFloat(rgbColorValue & 0x0000FF) / 255.0,
-            alpha: alpha
+            alpha: 1.0
         )
-    }
-    
-    // New initializer for the RecordDetailVM
-    convenience init?(hex: String) {
-        let r, g, b: CGFloat
-        
-        if hex.hasPrefix("#") {
-            let start = hex.index(hex.startIndex, offsetBy: 1)
-            let hexColor = String(hex[start...])
-            
-            if hexColor.count == 6 {
-                let scanner = Scanner(string: hexColor)
-                var hexNumber: UInt64 = 0
-                
-                if scanner.scanHexInt64(&hexNumber) {
-                    r = CGFloat((hexNumber & 0xff0000) >> 16) / 255
-                    g = CGFloat((hexNumber & 0x00ff00) >> 8) / 255
-                    b = CGFloat(hexNumber & 0x0000ff) / 255
-                    
-                    self.init(red: r, green: g, blue: b, alpha: 1.0)
-                    return
-                }
-            }
-        }
-        
-        return nil
     }
     
     static func fromHex(_ hex: String) -> UIColor? {
