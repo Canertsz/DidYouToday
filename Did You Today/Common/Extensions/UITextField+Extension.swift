b2a195d913836a97addc038f77b7a6be3f991ceb
Caner Tüysüz <canertuysuz@hotmail.com>
2025-04-01 12:15:48 +0300

    remove comment lines

--- Did You Today/Common/Extensions/UITextField+Extension.swift
+++ Did You Today/Common/Extensions/UITextField+Extension.swift
@@ -12,9 +12,6 @@ extension UITextField
     func addBottomBorder() {
         let bottomLine = CALayer()
         
-        // The border should be inside the text field
-        // so I changed frame.size.height + 5 to
-        // frame.size.height - 1
         bottomLine.frame = CGRect(x: 0,
                                   y: frame.size.height-1,
                                   width: frame.size.width,
@@ -23,11 +20,7 @@ extension UITextField
         bottomLine.backgroundColor = UIColor.darkGray.cgColor
         
         borderStyle = .none
-        
         layer.addSublayer(bottomLine)
-        
-        // Add this so the layer does not go beyond the
-        // bounds of the text field
         layer.masksToBounds = true
     }
 }
